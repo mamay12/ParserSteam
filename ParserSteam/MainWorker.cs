629e2abf28fcb3175df818facc247b487683d0d6
Ярослав <ytimchenko57@gmail.com>
2021-03-20 11:54:02 +0200

    Добавьте файлы проекта.

---
+++ ParserSteam/MainWorker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ParserSteam
+{
+    class MainWorker
+    {
+        string pathToSkin; decimal FLOAT, maxPrice; int count;
+        MainWorker(string _pathToSkins, decimal _float, decimal _maxPrice, int _count) {
+           
+        }
+
+        public void XPathFinderSingle(string xpath) 
+        {
+            
+        }
+        public void XPathFinderPlurality(string xpath)
+        {
+
+        }
+    }
+}
